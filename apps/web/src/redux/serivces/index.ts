ff91bd901dad6b26af56078305ec43c553472944
nikke-dev <95164611+nikke-dev@users.noreply.github.com>
2025-08-30 11:56:26 +0300

    Reading API Url from env

--- apps/web/src/redux/serivces/index.ts
+++ apps/web/src/redux/serivces/index.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../../utils/config';
+import { API_URL } from '../../utils/config';
 import { ReduxTagTypes } from '../tagTypes';
 
 const apiService = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
+    baseUrl: API_URL,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
 
