5f85adc49df3ce6d64d24fe584d9bc2772665f99
nikke-dev <95164611+nikke-dev@users.noreply.github.com>
2025-08-30 12:17:54 +0300

    Some config to backend

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,11 +3,12 @@ import helmet from 'helmet';
 import cors from 'cors';
 import { requestLogger } from './utils/middlewares';
 import apiV1Routes from './api/v1/routes';
+import { CORS_ORIGIN } from './utils/config';
 
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
 app.use(requestLogger);
 
--- apps/backend/src/utils/config.ts
+++ apps/backend/src/utils/config.ts
@@ -5,6 +5,7 @@ const env = cleanEnv(process.env, {
   PORT: num({ default: 3001 }),
   DATABASE_URL: str(),
   JWT_SECRET: str(),
+  CORS_ORIGIN: str({ default: '*' }),
 });
 
-export const { PORT, JWT_SECRET } = env;
+export const { PORT, JWT_SECRET, CORS_ORIGIN } = env;
